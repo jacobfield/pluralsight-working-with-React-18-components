9e176cb17893bc38e8f8b7c6d2dfd42af8004339
jacobfield <jacobprogrammes@gmail.com>
2024-09-17 11:12:00 +0100

    line through on completion added

--- todos/src/comps/ToDoItem.jsx
+++ todos/src/comps/ToDoItem.jsx
@@ -4,6 +4,7 @@ export default function ToDoItem({ text, completed, important }) {
   const [textState, setTextState] = useState(text);
   let date = new Date().toLocaleTimeString();
   const [dateState, setDateState] = useState(date);
+  const [completedState, setCompletedState] = useState(completed);
   // setting interval hook to update date every one second
   const interval = setInterval(() => {
     setDateState(new Date().toLocaleTimeString());
@@ -11,12 +12,33 @@ export default function ToDoItem({ text, completed, important }) {
   useEffect(() => {
     setTextState(`${text} ${date}`);
   }, [interval]);
+
+  function toggleLine(completedState) {
+    if (completedState !== true) {
+      setCompletedState(true);
+      console.log(completedState);
+    }
+    if (completedState) {
+      setCompletedState(false);
+      console.log(completedState);
+    }
+  }
+
   return (
     <>
-      <div className={completed ? "completed" : ""}>
+      <div className={completedState ? "completed" : ""}>
         {important ? "*" : ""}
         {textState}
+        <br></br>
+        <button onClick={() => toggleLine(completedState)}>Finished?</button>
       </div>
     </>
   );
 }
+
+// declare state for completed, set true or false.
+// set toggle function, which switches completed state
+// create button that for users to click, which toggles completed state: onClick={toggle}
+// add button to return statement
+// ternary operator to add completed class to div if completed is true
+// css class to add the strike out
